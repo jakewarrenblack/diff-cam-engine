bd95be480c131ff2be01b06359b30f6d55541d91
Jake <jakewarrenblack01@gmail.com>
2023-02-22 20:20:49 +0000

    Attempting to switch this over to using canvas instead of video, and not to bother requesting webcam access

--- diff-cam-engine.js
+++ diff-cam-engine.js
@@ -1,29 +1,29 @@
-var DiffCamEngine = (function() {
-	var stream;					// stream obtained from webcam
-	var video;					// shows stream
-	var captureCanvas;			// internal canvas for capturing full images from video
-	var captureContext;			// context for capture canvas
-	var diffCanvas;				// internal canvas for diffing downscaled captures
-	var diffContext;			// context for diff canvas
-	var motionCanvas;			// receives processed diff images
-	var motionContext;			// context for motion canvas
+const DiffCamEngine = (function() {
+	let canvas				// shows stream
+	let captureCanvas;			// internal canvas for capturing full images from video
+	let captureContext;			// context for capture canvas
+	let diffCanvas;				// internal canvas for diffing downscaled captures
+	let diffContext;			// context for diff canvas
+	let motionCanvas;			// receives processed diff images
+	let motionContext;			// context for motion canvas
 
-	var initSuccessCallback;	// called when init succeeds
-	var initErrorCallback;		// called when init fails
-	var startCompleteCallback;	// called when start is complete
-	var captureCallback;		// called when an image has been captured and diffed
+	let initSuccessCallback;	// called when init succeeds
+	let initErrorCallback;		// called when init fails
+	let startCompleteCallback;	// called when start is complete
+	let captureCallback;		// called when an image has been captured and diffed
 
-	var captureInterval;		// interval for continuous captures
-	var captureIntervalTime;	// time between captures, in ms
-	var captureWidth;			// full captured image width
-	var captureHeight;			// full captured image height
-	var diffWidth;				// downscaled width for diff/motion
-	var diffHeight;				// downscaled height for diff/motion
-	var isReadyToDiff;			// has a previous capture been made to diff against?
-	var pixelDiffThreshold;		// min for a pixel to be considered significant
-	var scoreThreshold;			// min for an image to be considered significant
-	var includeMotionBox;		// flag to calculate and draw motion bounding box
-	var includeMotionPixels;	// flag to create object denoting pixels with motion
+	let captureInterval;		// interval for continuous captures
+	let captureIntervalTime;	// time between captures, in ms
+	let captureWidth;			// full captured image width
+	let captureHeight;			// full captured image height
+	let diffWidth;				// downscaled width for diff/motion
+	let diffHeight;				// downscaled height for diff/motion
+	let isReadyToDiff;			// has a previous capture been made to diff against?
+	let pixelDiffThreshold;		// min for a pixel to be considered significant
+	let scoreThreshold;			// min for an image to be considered significant
+	let includeMotionBox;		// flag to calculate and draw motion bounding box
+	let includeMotionPixels;	// flag to create object denoting pixels with motion
+	let coords;
 
 	function init(options) {
 		// sanity check
@@ -32,13 +32,13 @@ var DiffCamEngine = (function() {
 		}
 
 		// incoming options with defaults
-		video = options.video || document.createElement('video');
+		canvas = options.canvas || document.createElement('canvas');
 		motionCanvas = options.motionCanvas || document.createElement('canvas');
 		captureIntervalTime = options.captureIntervalTime || 100;
-		captureWidth = options.captureWidth || 640;
-		captureHeight = options.captureHeight || 480;
-		diffWidth = options.diffWidth || 64;
-		diffHeight = options.diffHeight || 48;
+		captureWidth = canvas.width || 640;
+		captureHeight = canvas.height || 480;
+		diffWidth = canvas.width || 64;
+		diffHeight = canvas.height || 48;
 		pixelDiffThreshold = options.pixelDiffThreshold || 32;
 		scoreThreshold = options.scoreThreshold || 16;
 		includeMotionBox = options.includeMotionBox || false;
@@ -55,8 +55,6 @@ var DiffCamEngine = (function() {
 		diffCanvas = document.createElement('canvas');
 		isReadyToDiff = false;
 
-		// prep video
-		video.autoplay = true;
 
 		// prep capture canvas
 		captureCanvas.width = captureWidth;
@@ -73,22 +71,12 @@ var DiffCamEngine = (function() {
 		motionCanvas.height = diffHeight;
 		motionContext = motionCanvas.getContext('2d');
 
-		requestWebcam();
+		initSuccess()
 	}
 
-	function requestWebcam() {
-		var constraints = {
-			audio: false,
-			video: { width: captureWidth, height: captureHeight }
-		};
 
-		navigator.mediaDevices.getUserMedia(constraints)
-			.then(initSuccess)
-			.catch(initError);
-	}
 
-	function initSuccess(requestedStream) {
-		stream = requestedStream;
+	function initSuccess() {
 		initSuccessCallback();
 	}
 
@@ -98,40 +86,55 @@ var DiffCamEngine = (function() {
 	}
 
 	function start() {
-		if (!stream) {
-			throw 'Cannot start after init fail';
+		if (!canvas) {
+			throw "Can't do anything without a video canvas!" ;
 		}
 
 		// streaming takes a moment to start
-		video.addEventListener('canplay', startComplete);
-		video.srcObject = stream;
+		// video.addEventListener('canplay', startComplete);
+		// video.srcObject = stream;
+		startComplete()
 	}
 
 	function startComplete() {
-		video.removeEventListener('canplay', startComplete);
 		captureInterval = setInterval(capture, captureIntervalTime);
 		startCompleteCallback();
 	}
 
 	function stop() {
 		clearInterval(captureInterval);
-		video.src = '';
+		canvas.clearRect(0,0,canvas.width,canvas.height)
 		motionContext.clearRect(0, 0, diffWidth, diffHeight);
 		isReadyToDiff = false;
 	}
 
 	function capture() {
+		let self = this;
+
+		// Create an ImageData object to store the current frame from the canvas
+		let canvasData = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+
+		// If this is the first frame, store it as the background
+		if (!this.prevImageData) {
+			this.prevImageData = canvasData;
+			return;
+		}
+
+		// Store the current frame as the previous frame for the next iteration
+		this.prevImageData = canvasData;
+
+
 		// save a full-sized copy of capture
-		captureContext.drawImage(video, 0, 0, captureWidth, captureHeight);
-		var captureImageData = captureContext.getImageData(0, 0, captureWidth, captureHeight);
+		captureContext.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+		let captureImageData = captureContext.getImageData(0, 0, canvas.width, canvas.height);
 
 		// diff current capture over previous capture, leftover from last time
 		diffContext.globalCompositeOperation = 'difference';
-		diffContext.drawImage(video, 0, 0, diffWidth, diffHeight);
-		var diffImageData = diffContext.getImageData(0, 0, diffWidth, diffHeight);
+		diffContext.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+		let diffImageData = diffContext.getImageData(0, 0, canvas.width, canvas.height);
 
 		if (isReadyToDiff) {
-			var diff = processDiff(diffImageData);
+			let diff = processDiff(diffImageData);
 
 			motionContext.putImageData(diffImageData, 0, 0);
 			if (diff.motionBox) {
@@ -160,17 +163,17 @@ var DiffCamEngine = (function() {
 
 		// draw current capture normally over diff, ready for next time
 		diffContext.globalCompositeOperation = 'source-over';
-		diffContext.drawImage(video, 0, 0, diffWidth, diffHeight);
+		diffContext.drawImage(canvas, 0, 0, canvas.width, canvas.height);
 		isReadyToDiff = true;
 	}
 
 	function processDiff(diffImageData) {
-		var rgba = diffImageData.data;
+		let rgba = diffImageData.data;
 
 		// pixel adjustments are done by reference directly on diffImageData
-		var score = 0;
-		var motionPixels = includeMotionPixels ? [] : undefined;
-		var motionBox = undefined;
+		let score = 0;
+		let motionPixels = includeMotionPixels ? [] : undefined;
+		let motionBox = undefined;
 		for (var i = 0; i < rgba.length; i += 4) {
 			var pixelDiff = rgba[i] * 0.3 + rgba[i + 1] * 0.6 + rgba[i + 2] * 0.1;
 			var normalized = Math.min(255, pixelDiff * (255 / pixelDiffThreshold));
